91474d71fbc4a233b03f9a88e9e6386a53bc6b7c
DYNOH812 <munyuadennis@gmail.com>
2023-08-11 11:56:56 +0300

    Updates

---
+++ precache-manifest.77eec8d5569013324a08d9b7e8e3d7eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "338801454f1b6f7a6ee7",
+    "url": "/robofriends/static/css/main.b7094f5d.chunk.css"
+  },
+  {
+    "revision": "338801454f1b6f7a6ee7",
+    "url": "/robofriends/static/js/main.33880145.chunk.js"
+  },
+  {
+    "revision": "9259e4c9f1b2bcf38c10",
+    "url": "/robofriends/static/css/1.20c4d89f.chunk.css"
+  },
+  {
+    "revision": "9259e4c9f1b2bcf38c10",
+    "url": "/robofriends/static/js/1.9259e4c9.chunk.js"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "21eeda530a618050f62350572795e540",
+    "url": "/robofriends/index.html"
+  }
+];
